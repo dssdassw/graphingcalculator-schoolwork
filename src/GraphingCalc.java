64f5ac26ce20da389fde08cfb386b7279a57f56a
unknown <674843024@ntdm-134-12.hcdsb.edu.on.ca>
2016-02-29 12:51:29 -0500

    submitted work

--- src/GraphingCalc.java
+++ src/GraphingCalc.java
@@ -1,97 +1,136 @@
 import java.applet.*;
 import java.awt.*;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.util.Scanner;
-
-
-@SuppressWarnings("serial")
+@SuppressWarnings({ "serial", "unused" })
 
 public class GraphingCalc extends Applet {
 	public static int choice = -1; //I cant think of any other way to do this without having "Element doesnt exist", u wanna tussle? Will fix if have time.
+	double m, x, b, qa, qb, qc, qx, ca, cb, cc, cd, cx, endx;
+	Scanner in = new Scanner(System.in); //Same applies here
+	
 	public static double getLinear(double m, double x, double b) {
 		return (m*x+b); //forgot to remove the comment commenting on poor programming here, including another just to mock myself
 	}
+	
 	public static double getQuadratic(double a, double b, double c, double x) {
-		return (a*Math.pow(x, 2)+b*x+c);
+		return ((a*Math.pow(x, 2))+(b*x)+(c));
 	}
+	
 	public static double getCubic(double a, double b, double c, double d, double x) {
 		return (a*Math.pow(x, 3)+b*Math.pow(x, 2)+c*x+d);
 	}
+	
 	public static double[] getRange() { //just in case I want to use this
 		return new double[]{0, 1};
 	}
-	public static void graphLin(Graphics g, double m, double x, double b) {
-		System.out.println("Linear\n---------------\n");
-		System.out.print("Enter m, then x, then b for y = mx+b\n");
-		System.out.println("y = " + getLinear(m, x, b));
+	
+	public static void graphLin(Graphics2D g, double m, double x, double b, double endx, int w, int h) {
+		g.setStroke(new BasicStroke(3));
 		g.setColor(Color.red); //better dead than red? Maybe.
-		double todo=getLinear(m, x, b);
-		todo = getLinear(m, x+100, b);
-		System.out.println(todo);
-		g.translate((int)x, (int)getLinear(m, x, b));
-		g.drawLine((int) (x), (int)getLinear(m, (x), b), (int)x+100, (int)getLinear(m, x+100, b)); //a little convoluted but fuck it
+		g.rotate(180); //upside-down otherwise
+		g.translate((int)Math.round(w/2), (int)Math.round(h/2)); //sets origin to the middle of the window
+		g.drawLine((int)1, (int)Math.round(getLinear(m, 1, b)), (int)Math.round(endx), (int)Math.round(getLinear(m, endx, b)));
 	}
-	public static void graphQua(Graphics g) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Quadratic\n---------------\n");
-		System.out.print("For ax^2+bx+c, enter a, b, c, then x\n");
-		double qa = 1d; qa = in.nextDouble();
-		double qb = 2d; qb = in.nextDouble();
-		double qc = 3d; qc = in.nextDouble();
-		double qx = 4d; qx = in.nextDouble();
-		in.close();
-		System.out.println("y = " + getQuadratic(qa, qb, qc, qx));
+	
+	public static void graphQua(Graphics2D g, double a, double b, double c, double endx, int w, int h) {
+		double x = 0;
+		g.setStroke(new BasicStroke(3));
 		g.setColor(Color.green);
+		g.translate((int)Math.round(w/2), (int)Math.round(h/2));
+		g.rotate(180);
+		if(endx > 0d) { // - 
+			while(x <= endx) {
+				g.drawOval((int)Math.round(x), (int)Math.round(getQuadratic(a, b, c, x)), 1, 1);
+				x += 0.01;			
+			}
+			x = 0; //x is not 0 at the end of the other loop
+			while(x >= -endx) {
+				g.drawOval((int)Math.round(x), (int)Math.round(getQuadratic(a, b, c, x)), 1, 1);
+				x -= 0.01;
+			}
+		}
 	}
-	public static void graphCub(Graphics g) {
-		
+	
+	public static void graphCub(Graphics2D g, double a, double b, double c, double d, double endx, int w, int h) {
+		double x = endx-(2*endx);
+		g.setColor(Color.cyan); //System.out.println("y = " + );
+		g.setStroke(new BasicStroke(1));
+		g.translate((int)Math.round(w/2), (int)Math.round(h/2));
+		g.scale(2, 2);
+		g.rotate(-90); //unlike in the other two equations, the graph does not need to be fully turned around
+		if(endx > 0d) { // - 
+			while(x <= endx) {
+				double ans = getCubic(a, b, c, d, x);
+				System.out.println(x + " " + ans);
+				g.drawOval((int)Math.round(x), (int)Math.round(ans), 1, 1);
+				x += 0.01;
+			}
+		} else if(endx < 0d) { // +
+			while(x >= endx) {
+				g.drawOval((int)Math.round(x), (int)Math.round(getCubic(a, b, c, d, x)), 1, 1);
+				x -= 0.01;
+			}
+		}
 	}
-	public void paint(Graphics g) {
-		System.out.println("Painting");
-		Scanner in = new Scanner(System.in);
-		double m = 2d; double x = 1d; double b = 3d; double ca = 1d; double cb = 2d; double cc = 3d; double cd = 4d; double cx = 5d;
-		if(choice==-1) { //"init" and also SHITTY FORM
+	
+	public void paint(Graphics g) { //twitchwatchespokemon random puu.sh fest!
+		System.out.println("Painting..."); //http://puu.sh/noiVd/2a19c637cd.png http://puu.sh/noiVd/2a19c637cd.png
+		Graphics2D g2 = (Graphics2D) g; //http://puu.sh/noiYa/480ab20aa4.png http://puu.sh/noiYs/d5f2b25e0b.png
+		if(choice==-1) {
 			System.out.println("Choose an equation type\n1. Linear\n2. Quadratic\n3. Cubic\n(Input the number)\n");
 			choice = in.nextInt();
-			System.out.println(choice);
 			switch(choice) {
 				case 1:
-					System.out.println("Input m, then x, then b for y = mx+b");
+					System.out.println("Linear\n---------------\n");
+					System.out.println("Input m, then x, then b for y = mx+b\n");
 					m = in.nextDouble();
 					x = in.nextDouble();
 					b = in.nextDouble();
 					break;
 				case 2:
+					System.out.println("Quadratic\n---------------\n");
+					System.out.println("Input a, b, then c for ax^2 + bx + c\n");
+					qa = in.nextDouble();
+					qb = in.nextDouble();
+					qc = in.nextDouble();
 					break;
 				case 3:
+					System.out.println("Cubic\n---------------");
+					System.out.println("Input a, b, c, then d for ax^3 + bx^2 + cx + d");
+					ca = in.nextDouble();
+					cb = in.nextDouble();
+					cc = in.nextDouble();
+					cd = in.nextDouble();
 					break;
 			}
-			System.out.println(choice);
+			System.out.println("Choose an end value for x\n");
+			endx = in.nextDouble();
 		}
-		System.out.println(choice);
-		switch(choice) {
-			case 1:
-				graphLin(g, m, x, b);
-				break;
-			case 2:
-				break;
-			case 3:
-				System.out.println("Cubic\n---------------\n");
-				System.out.print("For ax^3+bx*2+cx+d, enter a, b, c, d, then x\n");
-				ca = in.nextDouble();
-				cb = in.nextDouble();
-				cc = in.nextDouble();
-				cd = in.nextDouble();
-				cx = in.nextDouble();
-				System.out.println("y = " + getCubic(ca, cb, cc, cd, cx));
-				break;
-			default: //in case the user is a cheeky bastard
-				System.out.println("Moronic\n---------------\nyou");
-				break;
+		if(choice == 1) {
+			System.out.println("x   y\n");
+			for(int i = 0; i<=endx; i++) { //prints table of values
+				System.out.println(i + " " + getLinear(m, (double)i, b));
+			}
+			graphLin(g2, m, x, b, endx, getWidth(), getHeight());
+		} else if(choice == 2) {
+			System.out.println("x   y\n");
+			for(int i = 0; i<=endx; i++) { //prints table of values
+				System.out.println(i + " " + getQuadratic(qa, qb, qc, (double)i));
+			}
+			graphQua(g2, qa, qb, qc, endx, getWidth(), getHeight());
+		} else if(choice == 3) {
+			System.out.println("x   y\n");
+			for(int i = 0; i<=endx; i++) { //prints table of values
+				System.out.println(i + " " + getCubic(ca, cb, cc, cd, (double)i));
+			}
+			graphCub(g2, ca, cb, cc, cd, endx, getWidth(), getHeight());
+		} else {
+			System.out.println("No input yet...");
 		}
 	}
+	
 	public static void main(String[] args) {
+		System.out.println("In main");
 		GraphingCalc calc = new GraphingCalc();
 		Graphics g = calc.getGraphics();
 	}
